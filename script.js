6c58b1878d52a3801408a6101d47c9cb4c957764
viniciusberlez-blip <vinicius.berlez@escola.pr.gov.br>
2025-08-06 11:27:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -12,6 +14,13 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -34,4 +43,137 @@ function contaFrequencias(palavras) {
         }
     }
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set([
+        "que",
+        "para",
+        "com",
+        "não",
+        "uma",
+        "por",
+        "mais",
+        "dos",
+        "como",
+        "mas",
+        "foi",
+        "ele",
+        "das",
+        "tem",
+        "seu",
+        "sua",
+        "ser",
+        "quando",
+        "muito",
+        "está",
+        "também",
+        "pelo",
+        "pela",
+        "até",
+        "isso",
+        "ela",
+        "entre",
+        "era",
+        "depois",
+        "sem",
+        "mesmo",
+        "aos",
+        "ter",
+        "seus",
+        "quem",
+        "nas",
+        "esse",
+        "eles",
+        "estão",
+        "você",
+        "tinha",
+        "foram",
+        "essa",
+        "num",
+        "nem",
+        "suas",
+        "meu",
+        "minha",
+        "têm",
+        "numa",
+        "pelos",
+        "elas",
+        "havia",
+        "seja",
+        "qual",
+        "será",
+        "nós",
+        "tenho",
+        "lhe",
+        "deles",
+        "essas",
+        "esses",
+        "pelas",
+        "este",
+        "fosse",
+        "dele",
+        "vocês",
+        "vos",
+        "lhes",
+        "meus",
+        "minhas",
+        "teu",
+        "tua",
+        "teus",
+        "tuas",
+        "nosso",
+        "nossa",
+        "nossos",
+        "nossas",
+        "dela",
+        "delas",
+        "esta",
+        "estes",
+        "estas",
+        "aquele",
+        "aquela",
+        "aqueles",
+        "aquelas",
+        "isto",
+        "aquilo",
+        "estou",
+        "estamos",
+        "estive",
+        "esteve",
+        "estivemos",
+        "estiveram",
+        "estava",
+        "estávamos",
+        "estavam",
+        "e",
+        "ou",
+        "onde",
+        "aquilo",
+        "embora",
+        "apesar",
+        "porque",
+        "enquanto",
+        "contudo",
+        "entretanto",
+        "portanto",
+        "além",
+        "antes",
+        "todavia",
+        "inclusive",
+        "diante",
+        "sobre",
+        "quanto",
+        "apenas",
+        "desde",
+        "uma vez",
+        "segundo",
+        "junto"
+    ]);
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
